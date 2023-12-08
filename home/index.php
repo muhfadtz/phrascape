5fbf9a1a43fa176d3fb5b06f9ce326a9d094b9b5
DP6ADAMFIRDAUS221401083 <114588658+DP6ADAMFIRDAUS221401083@users.noreply.github.com>
2023-12-09 06:57:40 +0700

    Update index.php

--- home/index.php
+++ home/index.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home / Phrascape</title>
-    <!-- Tautan Bootstrap CSS -->
+    <!-- Tautan Untuk Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
     <link rel="icon" href="../img/favicon.png" type="image/png">
